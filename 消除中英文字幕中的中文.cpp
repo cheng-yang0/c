6d830601847bc29b50dc1176144b2a03aa0aeebd
454477405 <34244968+454477405@users.noreply.github.com>
2019-03-15 20:30:00 +0800

    Add files via upload

---
+++ 消除中英文字幕中的中文.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h> 
+#define n 2
+int main()
+{
+	FILE *fp;
+	FILE *fp1;
+	char str[1000];
+	char str1[1000];
+	gets(str);
+	gets(str1);
+	fp=fopen(str,"rb+");
+	fp1=fopen(str1,"wb+");
+	unsigned short s[1];
+	//while(!feof(fp))
+	for(int i=0;i<1;i++)
+	{ 
+		fread(s,1,n,fp);
+		printf("%x",s[0]);
+		if(s[0]>=0X4E00&&s[0]<=0X9FA5||s[0]==0x7b)
+		;
+		else	
+		fwrite(s,1,n,fp1);
+	}
+	fclose(fp);
+	fclose(fp1);
+}
+//if(s[0]==)
+ 
